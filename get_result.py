8bcb5599136eb0ab3145f730a57f305bbb97b3c8
Khoi Bui <anhbuikhoi@gmail.com>
2026-04-01 13:00:37 +0900

    update

--- get_result.py
+++ get_result.py
@@ -2,6 +2,7 @@ import os
 import csv
 from tensorflow.python.summary.summary_iterator import summary_iterator
 
+
 def save_tensorboard_to_csv(tfevents_file, output_csv_file):
     """
     Extracts data from a TensorBoard log file and saves it to a CSV file.
@@ -15,7 +16,7 @@ def save_tensorboard_to_csv(tfevents_file, output_csv_file):
         raise FileNotFoundError(f"File not found: {tfevents_file}")
 
     # Open the CSV file for writing
-    with open(output_csv_file, mode='w', newline='') as csv_file:
+    with open(output_csv_file, mode="w", newline="") as csv_file:
         writer = csv.writer(csv_file)
         # Write the header
         writer.writerow(["Tag", "Step", "Value"])
@@ -27,7 +28,8 @@ def save_tensorboard_to_csv(tfevents_file, output_csv_file):
                     writer.writerow([value.tag, event.step, value.simple_value])
 
     print(f"Data saved to {output_csv_file}")
-    
+
+
 tfevents_path = "runs/Dec04_12-42-55_anda-Z690-AORUS-XTREME/events.out.tfevents.1733283775.anda-Z690-AORUS-XTREME.3625946.0"
 output_csv_path = "results/tensorboard_data_Dec04_12-42-55.csv"
 
